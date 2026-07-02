44e23956d2e5d3cc0265d00038fb1524575ac24c
Peter Wu <162184229+weirongw23-msft@users.noreply.github.com>
2026-07-02 09:52:26 -0400

    [Storage] [STG 104] NFS List V2 Files and Directories (#47643)

--- sdk/storage/azure-storage-file-share/azure/storage/fileshare/aio/_directory_client_async.py
+++ sdk/storage/azure-storage-file-share/azure/storage/fileshare/aio/_directory_client_async.py
@@ -591,7 +591,9 @@ class ShareDirectoryClient(AsyncStorageAccountHostsMixin, StorageAccountHostsMix
             begin with the specified prefix.
         :keyword List[str] include:
             Include this parameter to specify one or more datasets to include in the response.
-            Possible str values are "timestamps", "Etag", "Attributes", "PermissionKey".
+            Possible str values are "timestamps", "Etag", "Attributes", "PermissionKey",
+            "Permissions", "LinkCount", "NfsAttributes", and "All".
+            The values "Permissions", "LinkCount", and "NfsAttributes" apply only to NFS shares.
 
             .. versionadded:: 12.6.0
 
